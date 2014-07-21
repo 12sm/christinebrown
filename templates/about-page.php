dbd6ce9a215e64dfb283559bb663e08e75c06069
benhardt <ben@ben-hardt.com>
2014-07-21 15:42:23 -0500

    remove sidebar from news

---
+++ templates/about-page.php
@@ -0,0 +1,25 @@
+
+<h1>Bio</h1>
+
+<p>Christine discovered her love for the piano at an early age.  School years were spent accompanying vocal groups and taking piano lessons.  While earning a business degree at the University of Colorado, she was inspired by new age pianist George Winston, but never studied music or composition.  Years later, Christine rediscovered her passion for the piano, immersing herself in her music after the loss of her mother in 1990.  She began composing, using the piano to voice her emotions, releasing heartfelt compositions that captured listeners.
+Christine's earliest compositions were featured on 2 piano compilation albums, "Piano Portraits" and "Summer Stories" by Sugo Records.  Their success inspired her to record her own albums of original music.  Today, Christine continues to play and compose, and has released several CDs of solo piano, as well as Christmas albums and popular collections of children’s songs.  Her music can be heard worldwide, featured on Pandora, iTunes, Spotify, Whisperings Solo Piano Radio, Muzak, Sugo Music, cable and satellite channels, Healing Healthcare Systems, and is also distributed by CD Baby, Amazon and most major download sites. </p>
+
+ 
+
+<h1>Discography</h1> 
+ 
+Souvenirs (2014)
+A Classic Christmas (2012)
+The Wishing Well (2011)
+Childhood II (2011)
+ Promise (2009)
+Piano Stories (2006)
+Childhood (2004)
+Christmas On My Mind (2003)
+Believe (2003)
+Winter Tapestry (1997)
+ 
+ 
+Christine’s style of playing has classical influence with a new age sensitivity that often evokes strong emotions. Her graceful, elegant touch is gentle, yet powerful.“I am so grateful to be able to express myself musically, the piano being an emotional release, often an escape.  To move people with music and share it with others is truly rewarding.” Christine currently resides in San Diego, California with her husband and two sons.
+
+<h1>REVIEWS</h1> 
