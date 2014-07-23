339b37a497b9ef1d6f40fc9a339967956cbf65a7
benhardt <ben@ben-hardt.com>
2014-07-22 22:33:30 -0500

    header move

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,14 @@
-<!--<div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
-</div>-->
+<header class="masthead" role="banner">
+  <div class="container">
+  <div class="row">
+    <div class="col-md-12">
+    <div class="logo">
+    <a href="<?php echo home_url(); ?>/"><img class="img-responsive" src="/assets/img/cblogo.png"</img></a>
+    <div class="featured">
+    <?php echo do_shortcode('[wpv-view name="Featured News"]'); ?>
+    </div>
+    </div>
+    </div>
+  </div>
+  </div>
+</header>
