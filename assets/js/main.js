ea9f5f182e8e7b2cfc25afb120089c548bf8d35f
benhardt <ben@ben-hardt.com>
2014-07-29 18:01:00 -0500

    soundmanager.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,25 @@ var Roots = {
       } else {
        $.backstretch('../assets/img/bg.jpg');
     }
+  
+    soundManager.setup({
+    // disable or enable debug output
+    debugMode: true,
+    // use HTML5 audio for MP3/MP4, if available
+    preferFlash: false,
+    useFlashBlock: true,
+    // path to directory containing SM2 SWF
+    url: '/assets/js',
+    // optional: enable MPEG-4/AAC support (requires flash 9)
+    flashVersion: 9
+    });
+}
 
+
+soundManager.onready(function() {
+  // soundManager.createSound() etc. may now be called
+  inlinePlayer = new InlinePlayer();
+});   
 
 function affixnav(){
 
