f8485b847c2fd4de8b808eff7ade60e96417c59d
benhardt <ben@ben-hardt.com>
2014-07-29 15:49:11 -0500

    Revert "weird loop"
    This reverts commit 6d1921611c561490705d244f8ccced37843b9845.

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,8 +68,9 @@ var UTIL = {
 };
 
 $(document).ready(UTIL.loadEvents);
-if ($(window).width() >= 768){
+
         $(document).ready(function() {
+        
             $('#nav').affix({
       offset: {
         top: $('header').height()
@@ -89,7 +90,7 @@ if ($(window).width() >= 768){
             top: 17
              }
 }); 
-       } endif;
+        
         });
 
   $(document).ready(function() {
