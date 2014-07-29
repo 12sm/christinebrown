6fa5707723919f56a98d36a4c9d6c19a1f12e487
Ian Noyes <ian@12southmusic.com>
2014-07-29 16:44:23 -0500

    testing shit

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,7 @@ console.log('affixnav is firing');
         };
       }
       //Call affix on window resize
-      $(window).resize(affixnav());
+      $(window).resize(function(){console.log('widow resize');});
 
             }
   },
