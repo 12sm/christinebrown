e5f19fba21093ff2cf893c7173d52a0202412d5b
Ian Noyes <ian@12southmusic.com>
2014-07-31 16:12:19 -0500

    add trim

--- lib/custom.php
+++ lib/custom.php
@@ -1,4 +1,13 @@
-<?php 
+<?php
+
+add_shortcode('trim', 'trim_shortcode');
+function trim_shortcode($atts, $content = '') {
+  $content = wpv_do_shortcode($content);
+  if (strlen($content) > 20) {
+    $content = substr($content, 0, 20) . '…';
+  }
+  return $content;
+}
 
 define( 'UPLOADS', ''.'assets' );
 
