52fb2429e3af29f0c2f16cdf3e630711750f3acb
benhardt <ben@ben-hardt.com>
2014-07-30 19:02:07 -0500

    class

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -4,7 +4,7 @@
     <div class="col-md-12">
     <div class="logo">
     <a href="<?php echo home_url(); ?>/"><img class="img-responsive" src="/assets/img/cblogo.png"</img></a>
-    <div class="featured">
+    <div class="topfeatured">
      <?php echo do_shortcode('[wpv-view name="Featured News"]'); ?>
     </div>
     </div>
