70ce149f4b5bb8984693d50e217d47c8d393b208
Ian Noyes <ian@12southmusic.com>
2014-07-31 16:23:08 -0500

    trim loger

--- lib/custom.php
+++ lib/custom.php
@@ -4,8 +4,8 @@
 add_shortcode('trim', 'trim_shortcode');
 function trim_shortcode($atts, $content = '') {
   $content = wpv_do_shortcode($content);
-  if (strlen($content) > 8) {
-    $content = substr($content, 0, 8) . '…';
+  if (strlen($content) > 20) {
+    $content = substr($content, 0, 20) . '…';
   }
   return $content;
 }
