ebd95e00efc3e1d3d3cd10fbbaae7911d5b6dc66
Ian Noyes <ian@12southmusic.com>
2014-07-31 16:17:44 -0500

    less trim length

--- lib/custom.php
+++ lib/custom.php
@@ -3,8 +3,8 @@
 add_shortcode('trim', 'trim_shortcode');
 function trim_shortcode($atts, $content = '') {
   $content = wpv_do_shortcode($content);
-  if (strlen($content) > 20) {
-    $content = substr($content, 0, 20) . '…';
+  if (strlen($content) > 8) {
+    $content = substr($content, 0, 8) . '…';
   }
   return $content;
 }
