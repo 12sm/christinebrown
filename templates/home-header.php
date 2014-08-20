e5cae18a9289d827f1423eb3899b87dd290ff2f8
benhardt <ben@ben-hardt.com>
2014-08-20 11:51:13 -0500

    ccchnages

---
+++ templates/home-header.php
@@ -0,0 +1,14 @@
+<header class="masthead" role="banner">
+  <div class="container">
+  <div class="row">
+    <div class="col-md-12">
+    <div class="logo">
+    <a href="<?php echo home_url(); ?>/"><img class="img-responsive" src="/assets/img/cblogo.png"</img></a>
+    <div class="topfeatured">
+     <?php echo do_shortcode('[wpv-view name="Featured News"]'); ?>
+    </div>
+    </div>
+    </div>
+  </div>
+  </div>
+</header>  
